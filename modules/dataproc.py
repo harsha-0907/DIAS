59d9b64e8ab68f675e4d29dabecc26d417c1fe97
arakkutrip1612 <arakkutrip1612@gmail.com>
2025-02-13 19:06:02 +0000

    Modified changes to fix bug in dataproc.updateDataStore

--- modules/dataproc.py
+++ modules/dataproc.py
@@ -4,14 +4,20 @@ def updateDataStore(alerts):
     from data import dataStore
     # print(type(alerts))
     for alert in alerts:
+        flag = True
         city = alert.city
         dataStore[city]["alert_level"] = max(dataStore[city]["alert_level"], alert.alert_level)
         for index, alert_msg in enumerate(dataStore[city]["alerts"]):
+            print(alert_msg[1] == alert.message)
             if alert_msg[1] == alert.message:
                 # We have updated the already existing alert
                 dataStore[city]["alerts"][index][0] = alert.remove_after
-                continue
+                flag = False
+                break
         
         # If the alert is not present then add the alert & update the number of alerts
-        dataStore[city]["alerts"].append(alert.message)
-        dataStore[city]["numberofalerts"] = len(dataStore[city]["alerts"])
+        if flag is True:
+            dataStore[city]["alerts"].append([alert.remove_after, alert.message])
+            dataStore[city]["numberofalerts"] = len(dataStore[city]["alerts"])
+
+        print(dataStore[city]["alerts"])
